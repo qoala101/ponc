968ed4bdbfeef7fd7fe9602bf7be19f9a819d580
Volodymyr Hromakov <4y5t6r@gmail.com>
2023-02-15 23:14:13 +0200

    Updated settings fro demo.

--- src/draw/draw_menu_bar.cc
+++ src/draw/draw_menu_bar.cc
@@ -11,33 +11,45 @@ void MenuBar::Draw(State& state) {
   if (ImGui::BeginMainMenuBar()) {
     const auto menu_bar_scope = cpp::Scope{[]() { ImGui::EndMainMenuBar(); }};
 
-    if (ImGui::MenuItem("Open...")) {
-      open_file_dialog_.SetVisible(true);
+    if (ImGui::BeginMenu("File")) {
+      if (ImGui::MenuItem("Open...")) {
+        open_file_dialog_.SetVisible(true);
+      }
+
+      if (ImGui::MenuItem("Save As...")) {
+        save_as_file_dialog_.SetVisible(true);
+      }
+
+      if (ImGui::MenuItem("Reset")) {
+        state.PostEvent([](auto& state) { State::ResetDiagram(state); });
+      }
+
+      ImGui::EndMenu();
     }
 
-    if (ImGui::MenuItem("Save As...")) {
-      save_as_file_dialog_.SetVisible(true);
-    }
+    if (ImGui::BeginMenu("Views")) {
+      if (ImGui::MenuItem("Objects", nullptr, families_view_.IsVisible())) {
+        families_view_.SetVisible(!families_view_.IsVisible());
+      }
 
-    if (ImGui::MenuItem("Reset")) {
-      state.PostEvent([](auto& state) { State::ResetDiagram(state); });
-    }
+      if (ImGui::MenuItem("Flow Tree", nullptr, flow_tree_view_.IsVisible())) {
+        flow_tree_view_.SetVisible(!flow_tree_view_.IsVisible());
+      }
 
-    if (ImGui::MenuItem("Families", nullptr, families_view_.IsVisible())) {
-      families_view_.SetVisible(!families_view_.IsVisible());
-    }
+      if (ImGui::MenuItem("Groups", nullptr, groups_view_.IsVisible())) {
+        groups_view_.SetVisible(!groups_view_.IsVisible());
+      }
 
-    if (ImGui::MenuItem("Flow Tree", nullptr, flow_tree_view_.IsVisible())) {
-      flow_tree_view_.SetVisible(!flow_tree_view_.IsVisible());
-    }
+      if (ImGui::MenuItem("Group Settings", nullptr,
+                          group_settings_view_.IsVisible())) {
+        group_settings_view_.SetVisible(!group_settings_view_.IsVisible());
+      }
 
-    if (ImGui::MenuItem("Groups", nullptr, groups_view_.IsVisible())) {
-      groups_view_.SetVisible(!groups_view_.IsVisible());
-    }
+      if (ImGui::MenuItem("Settings", nullptr, settings_view_.IsVisible())) {
+        settings_view_.SetVisible(!settings_view_.IsVisible());
+      }
 
-    if (ImGui::MenuItem("Group Settings", nullptr,
-                        group_settings_view_.IsVisible())) {
-      group_settings_view_.SetVisible(!group_settings_view_.IsVisible());
+      ImGui::EndMenu();
     }
 
     if (ImGui::MenuItem("Zoom to Content")) {
@@ -48,10 +60,6 @@ void MenuBar::Draw(State& state) {
       for (const auto& link : state.app_.GetDiagram().GetLinks()) {
         ne::Flow(link.id);
       }
-    }
-
-    if (ImGui::MenuItem("Settings", nullptr, settings_view_.IsVisible())) {
-      settings_view_.SetVisible(!settings_view_.IsVisible());
     }
   }
 
