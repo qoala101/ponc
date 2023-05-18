3a5987144ad1a39531b1c7ee840b9f499bda035f
Volodymyr Hromakov <4y5t6r@gmail.com>
2023-05-18 20:26:53 +0300

    Added @link tags to copyright.

--- src/coreui/coreui_project.cc
+++ src/coreui/coreui_project.cc
@@ -1,5 +1,5 @@
 /**
- * PON Calculator https://github.com/qoala101/ponc
+ * PON Calculator @link https://github.com/qoala101/ponc @endlink
  * @author Volodymyr Hromakov (4y5t6r@gmail.com)
  * @copyright Copyright (c) 2023, MIT License
  */
