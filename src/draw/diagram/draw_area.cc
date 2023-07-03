c35e96e3139e679f4c48a0e7be4414ee41807fe1
Volodymyr Hromakov <4y5t6r@gmail.com>
2023-07-03 23:51:53 +0300

    Added drawing of area name.

--- src/draw/diagram/draw_area.cc
+++ src/draw/diagram/draw_area.cc
@@ -4,15 +4,33 @@
  * @copyright Copyright (c) 2023, MIT License
  */
 
-#include "draw_area.h"
-
+#include "draw_colored_text.h"
+#include "style_default_colors.h"
+#define IMGUI_DEFINE_MATH_OPERATORS
 #include <imgui.h>
+#include <imgui_internal.h>
 #include <imgui_node_editor.h>
 
 #include "core_area.h"
 #include "coreui_native_facade.h"
+#include "draw_area.h"
 
 namespace vh::ponc::draw {
+namespace {
+///
+void DrawAreaName(const core::Area &area) {
+  const auto color = ImColor{0, 0, 0, 255};
+  auto pos = ImVec2{0, -ImGui::CalcTextSize(area.name.c_str()).y};
+
+  if (ne::BeginGroupHint(area.id)) {
+    DrawColoredText(area.name, color, pos + ne::GetGroupMin());
+    ne::EndGroupHint();
+  } else {
+    DrawColoredText(area.name, color, pos + area.pos);
+  }
+}
+}  // namespace
+
 ///
 void DrawArea(core::Area &area, coreui::NodeMover &node_mover) {
   ImGui::PushID(area.id.AsPointer());
@@ -22,6 +40,8 @@ void DrawArea(core::Area &area, coreui::NodeMover &node_mover) {
   ne::EndNode();
 
   ImGui::PopID();
+
+  DrawAreaName(area);
 
   area.pos = ne::GetNodePosition(area.id);
   area.size = coreui::NativeFacade::GetAreaSize(area.id);
